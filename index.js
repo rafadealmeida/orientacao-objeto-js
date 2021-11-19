fac5f99385d2d99af01237343bb9dd6c6cb692ba
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2021-11-19 13:42:14 -0300

    Refatoração

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
-const cliente1 = new Cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
+const cliente1 = new Cliente("Ricardo",11122233309);
 
-const ContaCorrenteRicardo = new ContaCorrente();
-ContaCorrenteRicardo.agencia = 1001;
-ContaCorrenteRicardo.cliente = cliente1;
+const ContaCorrenteRicardo = new ContaCorrente(cliente1,1001);
 
-const cliente2 = new Cliente();
-cliente2.nome = "Aline";
-cliente2.cpf = 88822233309;
 
-const ContaCorrenteAline = new ContaCorrente();
-ContaCorrenteAline.agencia = 1002;
-ContaCorrenteAline.cliente = cliente2;
+const cliente2 = new Cliente("Aline",88822233309);
+
+const ContaCorrenteAline = new ContaCorrente(cliente2,1002);
 
 
 
 
-console.log(ContaCorrenteAline.cliente);
+console.log(cliente1);
 
 
 

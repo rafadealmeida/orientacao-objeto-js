2eb77f9795b9ba041ffbe075c97f8e18afcb0dec
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2021-11-19 18:51:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
 
 const cliente1 = new Cliente("Ricardo",11122233309);
 
 const ContaCorrenteRicardo = new ContaCorrente(cliente1,1001);
+ContaCorrenteRicardo.depositar(500);
+
+const contaPoupanca = new ContaPoupanca(50,cliente1,1001)
 
 
 const cliente2 = new Cliente("Aline",88822233309);
@@ -14,6 +18,10 @@ const ContaCorrenteAline = new ContaCorrente(cliente2,1002);
 
 
 console.log(cliente1);
+
+console.log(ContaCorrenteRicardo);
+
+console.log(contaPoupanca);
 
 console.log(ContaCorrente.numeroDeContas);
 

88414d86752477853df5ea4a8a517534f0689151
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2021-11-26 17:10:49 -0300

    Sobrescrevendo
    Sobrescrevendo  o método sacar , levando em consideração a taxa praticada pelo banco, nesse tipo de conta.

--- js-heranca-polimorfismo-aula1/ContaCorrente.js
+++ js-heranca-polimorfismo-aula1/ContaCorrente.js
@@ -7,4 +7,15 @@ export class ContaCorrente extends Conta{
         super(0,cliente,agencia);
         ContaCorrente.numeroDeContas += 1;
     } 
+    sacar(valor){
+        let taxa = 1.1;
+        const valorSacado = valor*taxa;
+        if(this._saldo>= valorSacado){
+            this._saldo -= valorSacado;
+            console.log(`${this._cliente.nome}, seu saldo é de ${this._saldo}`);
+            return valorSacado; 
+        }else{
+            console.log("Saldo inválido para a operação.");
+        }
+    }
 }

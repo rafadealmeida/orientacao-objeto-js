099fb07a7fb7e1d88e6e8d4d57f0c7357c19f4c9
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2021-11-26 19:02:27 -0300

    Método Abstrato no Sacar
    Assim evita do metodo ser chamado erroneamente , sem aplicar as devidas taxas do banco.

--- js-heranca-polimorfismo-aula1/Conta.js
+++ js-heranca-polimorfismo-aula1/Conta.js
@@ -1,8 +1,12 @@
 export class Conta{
     constructor(saldoInicial, cliente, agencia) {
+        if (this.constructor == "Conta") {
+            throw new Error("Você não deveria instanciar uma conta desse tipo diretamente.");
+        }
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
+       
     }
     
     set cliente(novoValor) {
@@ -20,8 +24,7 @@ export class Conta{
     }
 
     sacar(valor) {
-        let taxa = 1;
-        return this._saldo(valor, taxa);
+        throw new Error("O metodo Sacar da conta é abstrato.")
     }
 
      _sacar(valor,taxa){

6adfa5a84a62f3bbdf10ee46a3562c71d53fb3fb
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2021-11-26 16:36:17 -0300

    Ajuste na ordem constructor

--- js-heranca-polimorfismo-aula1/ContaCorrente.js
+++ js-heranca-polimorfismo-aula1/ContaCorrente.js
@@ -3,8 +3,8 @@ import {Conta} from "./Conta.js";
 export class ContaCorrente extends Conta{
     static numeroDeContas = 0;
 
-    constructor(agencia, cliente) {
-        super(0,agencia,cliente);
+    constructor( cliente,agencia) {
+        super(0,cliente,agencia);
         ContaCorrente.numeroDeContas += 1;
     } 
 }
